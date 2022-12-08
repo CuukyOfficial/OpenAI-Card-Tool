a71b00ce73d17b17a0c06becd391cff83c9822e1
JuliusDerErbauer <95232801+JuliusDerErbauer@users.noreply.github.com>
2022-12-08 13:43:28 +0100

    Update main
    multi line inputs, autoformating, validieren

--- main.py
+++ main.py
@@ -1,22 +1,68 @@
-import os
 import openai
 
-test = "Schreibe mir bitte deutsche Karteikarten im csv Dateiformat. Das bedeutet, dass die Frage " \
-       "der Karte gefolgt von einem ';' und dann die Antwort kommen soll. Die Karteikarten sollen Fragen zu folgendem " \
-       "Text stellen: \n\n {0}"
+def get_input():
+    # Liste zur Speicherung der Eingabe
+    input_lines = []
 
-openai.api_key = os.getenv("OPENAI_API_KEY")
+    # Schleife, die solange läuft, bis der Benutzer eine leere Eingabe macht
+    while True:
+        # Eingabe vom Benutzer abfragen
+        user_input = input()
 
-while True:
-    userInput = input("Gib den Text an, von welchem die AI Karteikarten generieren soll:")
-    print(test.format(userInput))
+        # Überprüfen, ob die Eingabe leer ist
+        if not user_input:
+            break
 
-    response = openai.Completion.create(
-        model="text-davinci-003",
-        prompt=test.format(userInput),
-        max_tokens=1000
-    )
+        # Die Eingabe zur Liste hinzufügen
+        input_lines.append(user_input)
 
-    f = open("cards.csv", 'a')
-    f.write(response.choices[0].text)
-    f.close()
+    # Die Liste als String zurückgeben
+    return "\n".join(input_lines)
+
+
+if __name__ == '__main__':
+    # API-Key für OpenAI setzen
+    openai.api_key = ""
+
+    # Vorlage für die Prompt-Nachricht
+    prompt_template = "Erstelle mir bitte deutsche Karteikarten im csv Dateiformat. Das bedeutet, dass auf jeder " \
+                      "Karteikarte eine Frage gefolgt von einem ';' und dann die Antwort dazu kommen soll. Schreibe " \
+                      "die Karteikarten zu dem folgenden Text: \n\n {0}"
+
+    # Endlosschleife zur wiederholten Abfrage von Benutzereingaben
+    while True:
+        print("Schreibe Nachricht:")
+
+        # Eingabe vom Benutzer abfragen
+        user_input = get_input()
+
+        # Nachricht für die OpenAI-API generieren
+        prompt_message = prompt_template.format(user_input)
+
+        print("Compiling...")
+
+        # Vervollständigung mit OpenAI anfordern
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=prompt_message,
+            max_tokens=1000,
+            temperature=1
+        )
+
+        # Antwort von OpenAI in cards-Variable speichern
+        cards = response.choices[0].text
+
+        # Liste der Zeichen, die ersetzt werden sollen
+        replace_chars = [("Frage; ", ""), ("Antwort; ", ""), ("Frage: ", ""), ("Antwort: ", ""), ("\n\n", "\n")]
+
+        # Schleife zum Durchlaufen der Zeichen in replace_chars
+        for old, new in replace_chars:
+            # Zeichen ersetzen
+            cards = cards.replace(old, new)
+
+        # Überprüfen, ob die Antwort mit einem Semikolon beginnt
+        if not cards.startswith(";"):
+            # Antwort in die Datei /Users/julianheines/Downloads/anki.csv schreiben
+            with open("/Users/julianheines/Downloads/anki.csv", 'a') as f:
+                f.write(cards)
+            print("Geschrieben")
